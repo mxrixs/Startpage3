059b31c0b100dea6a8d6f09b13bc4151801171e9
mxrixs <64852250+mxrixs@users.noreply.github.com>
2021-02-17 12:32:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function clock() {
     let date = new Date();
     let hrs = date.getHours();
     let mins = date.getMinutes();
-    let secs = date.getSeconds();
     
     hrs = hrs < 10 ? `0${hrs}` : hrs;
     mins = mins < 10 ? `0${mins}` : mins;
