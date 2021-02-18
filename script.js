65ef8447983327e72f677f12e7408bfb245a22f0
mxrixs <64852250+mxrixs@users.noreply.github.com>
2021-02-18 13:54:28 +0100

    Header changes

---
+++ script.js
@@ -0,0 +1,14 @@
+function clock() {
+    let date = new Date();
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    
+    hrs = hrs < 10 ? `0${hrs}` : hrs;
+    mins = mins < 10 ? `0${mins}` : mins;
+    
+    let time = `${hrs}:${mins}`;
+    setInterval(clock, 1000);
+    document.getElementById("clock").innerText = time;
+    }
+    
+    clock();
