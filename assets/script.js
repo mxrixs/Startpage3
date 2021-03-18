ede6eec35afb8c29d68a9b0775dd10a8302fd64c
mxrixs <64852250+mxrixs@users.noreply.github.com>
2021-03-18 20:58:41 +0100

    API WORKS

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,9 @@ var first = false;
 
 function weather(){
     console.log("weather() has been called");
-    var call = 'https://api.openweathermap.org/data/2.5/weather?id=2867714&appid=6fd57ceda43d5e2463dbe8d72e571bcd&units=metric';
-    fetch(call).then(res => res.json).then(handler);
+    const apikey = "6fd57ceda43d5e2463dbe8d72e571bcd"
+    var call = 'https://api.openweathermap.org/data/2.5/weather?id=2867714&appid=' + apikey + '&units=metric';
+    fetch(call).then(res => res.json()).then(handler);
 }
 weather();
 
@@ -20,44 +21,41 @@ function clock() {
     document.getElementById("clock").innerText = time;
 }
 clock();
-
-    
     
 function handler(res){
     
     console.log("starting...");
-
-    console.log(res);
+    const temperature = res.main.temp;
+    console.log(temperature);
     const identifier = res.weather[0].id;
     console.log(identifier);
-    const temperature = json.main.temp;
-    console.log(temperature);
+    
     
     console.log("using if...");
     
     if(identifier == 200 || identifier == 201 || identifier == 202){
-        document.GetElementById("Icon").src = "assets/icons/cloud-rain-lightning.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-rain-lightning.svg";
     }
     else if(identifier == 210 || identifier == 211 || identifier == 212 || identifier == 221){
-        document.GetElementById("Icon").src = "assets/icons/cloud-lightning.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-lightning.svg";
     }
     else if(identifier == 210 || identifier == 211 || identifier == 212 || identifier == 221){
-        document.GetElementById("Icon").src = "assets/icons/cloud-lightning.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-lightning.svg";
     }
     else if(identifier == 230 || identifier == 231 || identifier == 232){
-        document.GetElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
     }
     else if(identifier == 300 || identifier == 301 || identifier == 302 || identifier == 310 || identifier == 311 || identifier == 312 || identifier == 313 || identifier == 314 || identifier == 321){
-        document.GetElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-drizzle-lightning.svg";
     }
     else if(identifier == 500 || identifier == 501 || identifier == 520 || identifier == 521){
-        document.GetElementById("Icon").src = "assets/icons/cloud-rain.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-rain.svg";
     }
     else if(identifier == 502 || identifier == 503 || identifier == 504 || identifier == 520 || identifier == 521){
-        document.GetElementById("Icon").src = "assets/icons/cloud-rain-2.svg";
+        document.getElementById("Icon").src = "assets/icons/cloud-rain-2.svg";
     }
     else{
-        document.GetElementById("Icon").src = "assets/icons/error.svg"
+        document.getElementById("Icon").src = "assets/icons/error.svg"
     }
 
     console.log("temp is...");
